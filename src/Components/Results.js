1dde86cb7a9d52a573aa757ba9d563fecacbec4b
jaimie <moore.jaimie@gmail.com>
2019-03-05 15:34:35 -0500

    api info

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { link } from "fs";
+import YouTube from 'react-youtube';
 
 const apiKey = "220ba76687a248fe4b74726d993ed22f";
 
@@ -8,10 +8,14 @@ class Results extends Component {
   constructor() {
     super();
     this.state = {
-        movies:[]
-    }
+      movies: [],
+      description: "",
+      directors: '',
+      cast: '',
+      genres: '',
+      trailer: ""
+    };
   }
-  
 
   // async fetchMovies(){
   //     const movieData = await axios(`https://api.themoviedb.org/3/search/movie)
@@ -27,12 +31,12 @@ class Results extends Component {
       }
     }).then(response => {
       this.setState({
-          movies: response.data.results
-        });
-    })
+        movies: response.data.results
+      });
+    });
   }
 
-  movieInfoClick=(movieId)=> {
+  movieInfoClick = movieId => {
     axios({
       method: "get",
       url: `https://api.themoviedb.org/3/movie/${movieId}`,
@@ -42,37 +46,74 @@ class Results extends Component {
         append_to_response: "videos,credits"
       }
     }).then(response => {
-        console.log("hiiiii")
-        console.log(response)
+      const crew = response.data.credits.crew;
+      const cast = response.data.credits.cast;
+      const video = response.data.videos.results[0].key;
+      const genre = response.data.genres;
+
+      // These are the retunred info:
+
+      const description = response.data.overview;
+      const genreArray = genre
+        .map(name => {
+          return name.name;
+        }).join(", ");
+      const director = crew
+        .filter(member => {
+          return member.job === "Director";
+        })
+        .map(name => {
+          return name.name;
+        }).join(', ')
+      const topBilled = [];
+      for (let i = 0; i < 5; i++) {
+        topBilled.push(cast[i].name);
+      }
+      this.setState({
+        description: description,
+        directors: director,
+        cast: topBilled.join(", "),
+        genres: genreArray,
+        trailer: video
+      });
     });
-  }
-
-
-
+  };
 
   render() {
     return (
-        <header>
-            <h1>here</h1>
-            <ul>
-            {
-                this.state.movies.map((movie, i) => {
-                    let url = `http://image.tmdb.org/t/p/w185//${movie.poster_path}`
-                    return (
-                      <li key={i}>
-                        <p>{movie.title}</p>
-                        <img
-                        src={url}
-                        alt=""
-                        />
-                        <button onClick={()=>{this.movieInfoClick(movie.id)}}>More info</button>
-                      </li>
-                    );
-                })
-            }
-            </ul>
-        </header>
-    )
+      <header>
+        <h1>here</h1>
+        <ul>
+          {this.state.movies.map((movie, i) => {
+            let url = `http://image.tmdb.org/t/p/w185//${movie.poster_path}`;
+            return (
+              <li key={i}>
+                <p>{movie.title}</p>
+                <img src={url} alt="" />
+                <button
+                  onClick={() => {
+                    this.movieInfoClick(movie.id);
+                  }}
+                >
+                  More info
+                </button>
+                <div className="moreInfo">
+                <p>{this.description}</p>
+                <p>{this.cast}</p>
+                <p>{this.directors}</p>
+                <p>{this.genreArray}</p>
+                 <YouTube
+        videoId={this.video}
+        // opts={opts}
+        // onReady={this._onReady}
+      />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </header>
+    );
   }
 }
 
