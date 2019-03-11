f6ed760937252d3c81c558e23d268a0f2a88e146
Dan <dan_oconnor@live.ca>
2019-03-11 18:20:06 -0400

    more-info css

--- src/Components/MoreInfo.js
+++ src/Components/MoreInfo.js
@@ -1,19 +1,23 @@
-import React, { Component } from "react";
-import YouTube from "react-youtube";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component } from 'react';
+import YouTube from 'react-youtube';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faChevronCircleLeft,
   faPlusCircle
-} from "@fortawesome/free-solid-svg-icons";
-import ChoiceModal from "./ChoiceModal.js";
-import "../styles/moreInfo.css";
+} from '@fortawesome/free-solid-svg-icons';
+import ChoiceModal from './ChoiceModal.js';
+import '../styles/moreInfo.css';
 
 library.add(faChevronCircleLeft, faPlusCircle);
 
-const apiKey = "220ba76687a248fe4b74726d993ed22f";
+const apiKey = '220ba76687a248fe4b74726d993ed22f';
+const opts = {
+  height: "350",
+  width: '100%'
+};
 
 class MoreInfo extends Component {
   constructor(props) {
@@ -22,13 +26,13 @@ class MoreInfo extends Component {
     this.state = {
       //setting initial states
       movie: {},
-      description: "",
-      directors: "",
-      cast: "",
-      genres: "",
-      trailer: "",
+      description: '',
+      directors: '',
+      cast: '',
+      genres: '',
+      trailer: '',
       show: false,
-      posterPath: ""
+      posterPath: ''
     };
   }
   //functions to show / hide modals
@@ -39,27 +43,26 @@ class MoreInfo extends Component {
   };
   hideModal = () => {
     this.setState({
-      show: false,
-    })
-  }
+      show: false
+    });
+  };
   //go back to previous page
   goBack() {
     this.props.history.goBack();
   }
-  
 
   //make call to API for specific film details of movie that the user selected
   // this.props.match.params.movieId -> when we click on the image, the movie Id goes into the URL and this grabs it from the URL and uses it for the axios call.
   componentDidMount = () => {
     axios({
-      method: "get",
+      method: 'get',
       url: `https://api.themoviedb.org/3/movie/${
         this.props.match.params.movieId
       }`,
-      responseType: "json",
+      responseType: 'json',
       params: {
         api_key: apiKey,
-        append_to_response: "videos,credits"
+        append_to_response: 'videos,credits'
       }
     }).then(response => {
       const crew = response.data.credits.crew;
@@ -73,15 +76,15 @@ class MoreInfo extends Component {
         .map(name => {
           return name.name;
         })
-        .join(", ");
+        .join(', ');
       const director = crew
         .filter(member => {
-          return member.job === "Director";
+          return member.job === 'Director';
         })
         .map(name => {
           return name.name;
         })
-        .join(", ");
+        .join(', ');
       const topBilled = [];
       for (let i = 0; i < 5; i++) {
         topBilled.push(cast[i].name);
@@ -91,7 +94,7 @@ class MoreInfo extends Component {
         movie: response.data,
         description: description,
         directors: director,
-        cast: topBilled.join(", "),
+        cast: topBilled.join(', '),
         genres: genreString,
         trailer: video
       });
@@ -100,46 +103,54 @@ class MoreInfo extends Component {
   // using state to create a div with more info (title, tagline, trailer, overview, cast, directors, genres) about the selected film
   render() {
     return (
-      <div className="moreInfo">
-        <div>
-          <FontAwesomeIcon onClick={this.goBack} icon="chevron-circle-left" />
-          <FontAwesomeIcon onClick={this.showModal} icon="plus-circle" />
-          {this.state.show && (
-            <ChoiceModal
-              title={this.state.movie.title}
-              poster={this.state.movie.poster_path}
-              duration={this.state.movie.runtime}
-              genre={this.state.genres}
-              movieId={this.props.match.params.movieId}
-              handleClose={this.hideModal}
+      <div className='moreInfo'>
+        <div className='wrapper'>
+          <div className='moreInfoIcons clearfix'>
+            <FontAwesomeIcon
+              onClick={this.goBack}
+              icon='chevron-circle-left'
             />
-          )}
+            <FontAwesomeIcon onClick={this.showModal} icon='plus-circle' />
+            {this.state.show && (
+              <ChoiceModal
+                title={this.state.movie.title}
+                poster={this.state.movie.poster_path}
+                duration={this.state.movie.runtime}
+                genre={this.state.genres}
+                movieId={this.props.match.params.movieId}
+                handleClose={this.hideModal}
+              />
+            )}
 
-
-          <h2>{this.state.movie.title}</h2>
-          <h3>{this.state.movie.tagline}</h3>
-        </div>
-        <YouTube
-          videoId={this.state.trailer}
-          // these are the options for the video opts={opts} onReady={this._onReady}
-        />
-        <div>
-          <p>
-            <span>Description:</span>
-            {this.state.movie.overview}
-          </p>
-          <p>
-            <span>Cast:</span>
-            {this.state.cast}
-          </p>
-          <p>
-            <span>Directors:</span>
-            {this.state.directors}
-          </p>
-          <p>
-            <span>Genres:</span>
-            {this.state.genres}
-          </p>
+            <h2>{this.state.movie.title}</h2>
+            <h3>{this.state.movie.tagline}</h3>
+          </div>
+          <div>
+            <YouTube
+              videoId={this.state.trailer}
+              opts={opts}
+              containerClassName={'youTubeContainer'}
+              // these are the options for the video opts={opts} onReady={this._onReady}
+            />
+          </div>
+          <div className='infoWrap'>
+            <p>
+              <span>Description: </span>
+              {this.state.movie.overview}
+            </p>
+            <p>
+              <span>Cast: </span>
+              {this.state.cast}
+            </p>
+            <p>
+              <span>Directors: </span>
+              {this.state.directors}
+            </p>
+            <p>
+              <span>Genres: </span>
+              {this.state.genres}
+            </p>
+          </div>
         </div>
       </div>
     );
