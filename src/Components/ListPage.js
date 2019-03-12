3446e43fb01f2e83ec442e185e3ae0ee779e3ee1
Dan <dan_oconnor@live.ca>
2019-03-12 15:36:23 -0400

    updating css

--- src/Components/ListPage.js
+++ src/Components/ListPage.js
@@ -1,17 +1,13 @@
-import React, { Component } from "react";
-import firebase from "../firebase.js";
-import Modal from "./Modal.js";
-import {Link} from "react-router-dom";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
-import "../styles/listPage.css";
-
+import React, { Component } from 'react';
+import firebase from '../firebase.js';
+import Modal from './Modal.js';
+import { Link } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import '../styles/listPage.css';
 
 library.add(faTimesCircle);
-
-
-
 
 class ListPage extends Component {
   constructor() {
@@ -19,14 +15,14 @@ class ListPage extends Component {
     //setting initial state
     this.state = {
       lists: [],
-      listsName: "",
+      listsName: '',
       show: false
     };
   }
   //connecting to firebase, we're grabbing the user's list names and setting them to a temporary array, and then setting state of the lists array to the temporary array.
   componentDidMount() {
     const dbRef = firebase.database().ref();
-    dbRef.on("value", res => {
+    dbRef.on('value', res => {
       console.log(res.val());
       let response = res.val();
       let tempArray = [];
@@ -52,11 +48,11 @@ class ListPage extends Component {
   };
 
   //removeList from List Page
-  removeList = (listName) => {
+  removeList = listName => {
     {
-      const dbRef = firebase.database().ref()
-      let matchedObject = ''
-      dbRef.on("value", res => {
+      const dbRef = firebase.database().ref();
+      let matchedObject = '';
+      dbRef.on('value', res => {
         let response = res.val();
         //going through each object in database response, checking to see if list names match, once we get to the list name that is the same as the list name we clicked on, remove that list from the database.
         for (let object in response) {
@@ -66,37 +62,46 @@ class ListPage extends Component {
         }
       });
       const listRef = dbRef.child(matchedObject);
-      listRef.remove()
+      listRef.remove();
     }
-  }
+  };
 
   //on click of create new list, modal appears for user to enter list name, also this page is displaying the lists the user already has.
   render() {
     return (
-      <div className="listPage listPageWrapper">
+      <div className='listPageWrapper'>
         <h2>Movie Lists</h2>
-        <Link to="/results" className="buttonStyle">Search Movies</Link>
-        <a className="buttonStyle" onClick={this.showModal}>
-          Create new list
-        </a>
-        <p className="instructions">
+        <div className='listNavButtonWrap clearfix'>
+          <Link to='/results' className='buttonStyle'>
+            Search Movies
+          </Link>
+          <a className='buttonStyle' onClick={this.showModal}>
+            Create new list
+          </a>
+        </div>
+        <p className='instructions'>
           Welcome to the movie lists page! Select an existing list below to view
-          movies. Help the Quick Flicks community by adding movies to an existing list, or creating your own list to  add all of your favourite flicks.{" "}
+          movies. Help the Quick Flicks community by adding movies to an
+          existing list, or creating your own list to add all of your favourite
+          flicks.{' '}
         </p>
         {this.state.show && <Modal handleClose={this.hideModal} />}
         <h3>Existing Lists:</h3>
-        <ul className="">
+        <ul>
           {this.state.lists.map((listName, i) => {
             return (
-              <li key={i} className="listName clearfix">
-                <Link to={`/lists/${listName}`}>
+              <li key={i} className='listNames clearfix'>
+                <Link
+                  className='buttonStyle'
+                  to={`/lists/${listName}`}
+                >
                   {/* adding space back to list names displayed on list page */}
-                  {listName.replace(/-/g, " ")}
+                  {listName.replace(/-/g, ' ')}
                 </Link>
 
                 <FontAwesomeIcon
-                  className="removeList child"
-                  icon="times-circle"
+                  className='removeList child'
+                  icon='times-circle'
                   onClick={() => {
                     this.removeList(listName);
                   }}
