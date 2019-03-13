70b9b97770a8c25f05a6ee3d3ca915e8c37b5c38
Jenna <jenna.maask@gmail.com>
2019-03-13 09:18:12 -0400

    finishing header styling and final clean up

--- src/Components/SpecificList.js
+++ src/Components/SpecificList.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
-import { Link } from 'react-router-dom';
-import NatLangForm from './NatLangForm.js';
-import Footer from './Footer.js';
-import '../styles/specificList.css';
+import React, { Component } from "react";
+import firebase from "firebase";
+import { Link } from "react-router-dom";
+import NatLangForm from "./NatLangForm.js";
+import Footer from "./Footer.js";
+import "../styles/specificList.css";
 
 class SpecificList extends Component {
   constructor() {
@@ -11,16 +11,16 @@ class SpecificList extends Component {
     //setting state
     this.state = {
       listMovies: [],
-      title: '',
+      title: "",
       imageSize: 0
     };
   }
 
   componentDidMount() {
     this.updateImageSize();
-    window.addEventListener('resize', this.updateImageSize.bind(this));
+    window.addEventListener("resize", this.updateImageSize.bind(this));
     const dbRef = firebase.database().ref();
-    dbRef.on('value', res => {
+    dbRef.on("value", res => {
       let tempArray = [];
       const response = res.val();
       //find which object in our database matches the name of the list we have clicked on
@@ -33,19 +33,18 @@ class SpecificList extends Component {
       }
       this.setState({
         listMovies: tempArray,
-        title: this.props.match.params.listName.replace(/-/g, ' ')
+        title: this.props.match.params.listName.replace(/-/g, " ")
       });
     });
   }
 
   updateImageSize = () => {
-    let imageSize = '342';
+    let imageSize = "342";
     if (window.innerWidth > 1600) {
-      imageSize = '780';
+      imageSize = "780";
     } else if (window.innerWidth > 1000) {
-      imageSize = '500';
+      imageSize = "500";
     }
-
     this.setState({
       imageSize: imageSize
     });
@@ -53,8 +52,8 @@ class SpecificList extends Component {
   //remove movie from list
   removeMovie = movieId => {
     const dbRef = firebase.database().ref();
-    let matchedObject = '';
-    dbRef.on('value', res => {
+    let matchedObject = "";
+    dbRef.on("value", res => {
       let response = res.val();
       //going through each object in database response, checking to see if list names match, once we get to the list name that is the same as the list name we clicked on, we go into the object to determine if it has a movie array already.
       for (let object in response) {
@@ -65,46 +64,34 @@ class SpecificList extends Component {
     });
     const listRef = dbRef.child(matchedObject);
     listRef
-      .child('movies')
+      .child("movies")
       .child(movieId)
       .remove();
   };
 
-  openMenu = () => {
-    this.setState({
-      showMenu: true
-    });
-  };
-
-  closeMenu = () => {
-    this.setState({
-      showMenu: false
-    });
-  };
-
   render() {
     return (
-      <div className='specificList'>
-        <div className='wrapper clearfix'>
+      <div className="specificList">
+        <div className="wrapper clearfix">
           <h2>{this.state.title}</h2>
-          <nav className='bigNav clearfix'>
-            <Link className='buttonStyle' to='/results'>
+          <nav className="bigNav clearfix">
+            <Link className="buttonStyle" to="/results">
               Search Movies
             </Link>
-            <Link className='buttonStyle' to='/lists'>
+            <Link className="buttonStyle" to="/lists">
               Lists
             </Link>
           </nav>
         </div>
         {this.state.listMovies.length === 0 ? (
-          <div className='wrapper'>
-            <p className='empty'>
+          <div className="wrapper">
+            <p className="empty">
               Looks like nobody has added any movies to this list yet!
             </p>
           </div>
         ) : (
-          <div className='movieList clearfix'>
-            <div className='wrapper'>
+          <div className="movieList clearfix">
+            <div className="wrapper">
               <NatLangForm movieInfo={this.state.listMovies} />
             </div>
             {this.state.listMovies.map(movieId => {
@@ -112,12 +99,12 @@ class SpecificList extends Component {
                 movieId.poster
               }`;
               return (
-                <div className='poster'>
+                <div className="poster">
                   <img src={url} alt={`Poster of ${movieId.name}`} />
-                  <div className='overlay'>
+                  <div className="overlay">
                     <Link to={`/movies/${movieId.id}`}>More Info</Link>
                     <button
-                      className='removeButton'
+                      className="removeButton"
                       onClick={() => {
                         this.removeMovie(movieId.id);
                       }}
