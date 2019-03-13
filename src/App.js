cb31dc0a7d228d67d2acc3b4dca007937f07c901
Dan <dan_oconnor@live.ca>
2019-03-13 09:09:47 -0400

    more-info & choiceModal css changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import "./App.css";
-import Results from "./Components/Results.js";
-import MoreInfo from "./Components/MoreInfo.js";
-import ListPage from "./Components/ListPage.js";
-import SpecificList from "./Components/SpecificList";
-import Footer from './Components/Footer';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Results from './Components/Results.js';
+import MoreInfo from './Components/MoreInfo.js';
+import ListPage from './Components/ListPage.js';
+import SpecificList from './Components/SpecificList';
 import Header from './Components/Header.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: ""
+      searchResults: ''
     };
   }
   //Function sets users search input as the state in the app
@@ -28,11 +27,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-
-        <Route path="/" exact component={Header} />
-        <Route
-            path="/results"
+        <div className='App'>
+          <Route path='/' exact component={Header} />
+          <Route
+            path='/results'
             exact
             render={() => {
               return (
@@ -43,9 +41,9 @@ class App extends Component {
               );
             }}
           />
-          <Route path="/movies/:movieId" component={MoreInfo} />
-          <Route path="/lists" exact component={ListPage} />
-          <Route path="/lists/:listName" component={SpecificList} />
+          <Route path='/movies/:movieId' component={MoreInfo} />
+          <Route path='/lists' exact component={ListPage} />
+          <Route path='/lists/:listName' component={SpecificList} />
         </div>
       </Router>
     );
